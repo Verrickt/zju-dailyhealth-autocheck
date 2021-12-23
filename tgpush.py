b91162ab6b64c211cd9e915bdf13ce8d90a97add
Von <>
2021-12-23 13:21:19 +0800

    Update

--- tgpush.py
+++ tgpush.py
@@ -5,14 +5,12 @@ import os
 TG 消息推送模块
 """
 
-TG_TOKEN = os.getenv("TG_TOKEN")	#TG机器人的TOKEN
-CHAT_ID = os.getenv("CHAT_ID")	    #推送消息的CHAT_ID
-
 
 
 def post_tg(config,message):
     TG_TOKEN = config['bot_token']
     CHAT_ID = config['chat_id']
+    PROXY = config['proxy']
     telegram_message = f"{message}"
     params = (
         ('chat_id', CHAT_ID),
@@ -21,13 +19,15 @@ def post_tg(config,message):
         ('disable_web_page_preview', "yes")
     )    
     telegram_url = "https://api.telegram.org/bot" + TG_TOKEN + "/sendMessage"
-    telegram_req = post(telegram_url, params=params)
+    proxies = {
+        'https':PROXY,
+        'http': PROXY
+    }
+    telegram_req = post(telegram_url, params=params,proxies=proxies)
     telegram_status = telegram_req.status_code
     if telegram_status == 200:
         print(f"INFO: Telegram Message sent")
     else:
         print("Telegram Error")
-        
-if __name__ == "__main__":
-    post_tg('浙江大学每日健康打卡 V1.0 '+ " \n\n 签到结果: " + "请自行查看")    
+
  
